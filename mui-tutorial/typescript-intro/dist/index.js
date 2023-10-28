0d410aa1fe99daf7dfc53ea470091bcad12c6da6
darrenfdes <darrenmfdes@gmail.com>
2023-10-28 15:10:39 +0530

    ran tsc on modified example

--- mui-tutorial/typescript-intro/dist/index.js
+++ mui-tutorial/typescript-intro/dist/index.js
@@ -1,28 +1,48 @@
 "use strict";
-const addStock = (portfolio, stock) => {
-    portfolio.push(stock);
+const addPortfolioItem = (portfolio, item) => {
+    return [...portfolio, item];
 };
 const calculatePortfolioValue = (portfolio) => {
     let totalValue = 0;
-    for (const stock of portfolio) {
-        totalValue += stock.price;
+    for (const item of portfolio) {
+        if ("price" in item) {
+            totalValue += item.price;
+        }
+        else if ("faceValue" in item) {
+            totalValue += item.faceValue;
+        }
     }
     return totalValue;
 };
 const displayPortfolio = (portfolio) => {
-    console.log("Stock Portfolio:");
-    for (const stock of portfolio) {
-        console.log(`Symbol: ${stock.symbol} - Name: ${stock.name} - Price: $${stock.price}`);
+    console.log("Portfolio Items:");
+    for (const item of portfolio) {
+        if ("price" in item) {
+            console.log(`Symbol: ${item.symbol} - Name: ${item.name} - Price: $${item.price}`);
+        }
+        else if ("faceValue" in item) {
+            console.log(`Symbol: ${item.symbol} - Name: ${item.name} - Face Value: $${item.faceValue}`);
+        }
     }
     console.log(`Total Portfolio Value: $${calculatePortfolioValue(portfolio)}`);
 };
 const apple = { symbol: "AAPL", name: "Apple Inc.", price: 150.25 };
 const google = { symbol: "GOOGL", name: "Alphabet Inc.", price: 2750.1 };
-const amazon = { symbol: "AMZN", name: "Amazon.com Inc.", price: 3500.75 };
-const microsoft = { symbol: "MSFT", name: "Microsfot", price: 3500.75 };
-const portfolio = [];
-addStock(portfolio, apple);
-addStock(portfolio, google);
-addStock(portfolio, amazon);
-addStock(portfolio, microsoft);
+const microsoft = { symbol: "MSFT", name: "Microsoft", price: 3500.75 };
+const usTreasuryBond = {
+    symbol: "USTB",
+    name: "U.S. Treasury Bond",
+    faceValue: 1000,
+};
+const corporateBond = {
+    symbol: "CORP",
+    name: "Corporate Bond",
+    faceValue: 500,
+};
+let portfolio = [];
+portfolio = addPortfolioItem(portfolio, apple);
+portfolio = addPortfolioItem(portfolio, google);
+portfolio = addPortfolioItem(portfolio, microsoft);
+portfolio = addPortfolioItem(portfolio, usTreasuryBond);
+portfolio = addPortfolioItem(portfolio, corporateBond);
 displayPortfolio(portfolio);
